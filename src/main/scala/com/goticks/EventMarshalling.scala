b11f1796939b1e56b3c166d0e54e914f592129cc
provalentin <provalentin@gmail.com>
2018-05-12 15:29:33 +0300

    Update EventMarshalling.scala

--- src/main/scala/com/goticks/EventMarshalling.scala
+++ src/main/scala/com/goticks/EventMarshalling.scala
@@ -3,7 +3,9 @@ package com.goticks
 import spray.json._
 
 trait EventMarshalling extends DefaultJsonProtocol {
-  implicit val stringFormat = stringJsonFormat
+  import BoxOffice._
+  implicit val eventsFormat = jsonFormat1(Events)
+  implicit val eventFormat = jsonFormat2(Event)
 
 }
 
