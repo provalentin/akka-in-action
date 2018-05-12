3d053ca5fdb7d6a424a85c28c98e2892be6360f7
provalentin <provalentin@gmail.com>
2018-05-12 15:33:12 +0300

    Update RestApi.scala

--- src/main/scala/com/goticks/RestApi.scala
+++ src/main/scala/com/goticks/RestApi.scala
@@ -2,6 +2,7 @@ package com.goticks
 
 import akka.actor.ActorSystem
 import akka.actor.ActorRef
+import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server._
