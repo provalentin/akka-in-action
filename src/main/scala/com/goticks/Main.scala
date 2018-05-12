05fdc28deb4e98b6c82db5f600ae02b43557cb30
provalentin <provalentin@gmail.com>
2018-05-12 13:07:50 +0300

    Update Main.scala

--- src/main/scala/com/goticks/Main.scala
+++ src/main/scala/com/goticks/Main.scala
@@ -18,7 +18,7 @@ object Main extends App with RequestTimeout {
 }
 
 trait RequestTimeout {
-  import scala.concurent.duration._
+  import scala.concurrent.duration._
   def requestTimeout(config: Config):Timeout = {
     val t = config.getString("akka.http.server.request-timeout")
     val d = Duration(t)
