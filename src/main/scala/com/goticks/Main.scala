a2a67701591fe966d466ebd5201c8850320fa92a
provalentin <provalentin@gmail.com>
2018-05-12 13:06:39 +0300

    Update Main.scala

--- src/main/scala/com/goticks/Main.scala
+++ src/main/scala/com/goticks/Main.scala
@@ -1,11 +1,27 @@
 package com.goticks
 
+import akka.actor.ActorSystem
+import akka.util.Timeout
+
+
 import com.typesafe.config.{Config, ConfigFactory}
 
-object Main extends App {
+object Main extends App with RequestTimeout {
   val config = ConfigFactory.load()
   val host = config.getString("http.host")
   val port = config.getInt("http.port")
   
   println(s"Config => host: $host, port: $port")
+  
+  implicit val system = ActorSystem()
+  implicit val ec = system.dispatcher
 }
+
+trait RequestTimeout {
+  import scala.concurent.duration._
+  def requestTimeout(config: Config):Timeout = {
+    val t = config.getString("akka.http.server.request-timeout")
+    val d = Duration(t)
+    FiniteDuration(d.lenght, d.unit)
+  }
+}  
