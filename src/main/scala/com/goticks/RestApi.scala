1a064a5cdbaf272e0dd498021b8406ec87f37bf1
provalentin <provalentin@gmail.com>
2018-05-12 14:32:04 +0300

    Update RestApi.scala
    add RestRoutes trait

--- src/main/scala/com/goticks/RestApi.scala
+++ src/main/scala/com/goticks/RestApi.scala
@@ -4,8 +4,13 @@ import akka.actor.ActorSystem
 import akka.util.Timeout
 
 
-class RestApi(system: ActorSystem, timeout: Timeout) {
+class RestApi(system: ActorSystem, timeout: Timeout) extends RestRoutes {
   implicit def executionContext = system.dispatcher
   implicit val requestTimeout = timeout
   //def createBoxOffice = system.actorOf(BoxOffice.props, BoxOffice.name)
+}
+
+trait RestRoutes {
+ 
+  def routes: Route
 }
