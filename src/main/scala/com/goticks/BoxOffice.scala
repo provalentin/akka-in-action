a361794bbf3dbda8eaff27e3be860c02813cbe0d
provalentin <provalentin@gmail.com>
2018-05-12 15:52:57 +0300

    Update BoxOffice.scala

--- src/main/scala/com/goticks/BoxOffice.scala
+++ src/main/scala/com/goticks/BoxOffice.scala
@@ -1,10 +1,17 @@
 package com.goticks
 
+import akka.actor._
+
 object BoxOffice {
-  
+  def props(implicit timeout: Timeout) = Props(new BoxOffice())
+  def name = "boxOffice"
   case object GetEvents
   
   case class Event(name: String, tickets: Int)
   case class Events(events: Vector[Event])
+  
+}
+
+class BoxOffice(imlicit timeout: Timeout) extends Actor {
   
 }
