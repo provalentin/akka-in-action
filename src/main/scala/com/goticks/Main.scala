9febe11ba21dc89ceb85add276d1086147c086d7
provalentin <provalentin@gmail.com>
2018-05-12 12:46:49 +0300

    Update Main.scala

--- src/main/scala/com/goticks/Main.scala
+++ src/main/scala/com/goticks/Main.scala
@@ -1,5 +1,11 @@
+package com.goticks
 
+import com.typesafe.config.{Config, ConfigFactory}
 
 object Main extends App {
-
+  val config = ConfigFactory.load()
+  val host = config.getString("http.host")
+  val port = config.getInt("http.port")
+  
+  
 }
