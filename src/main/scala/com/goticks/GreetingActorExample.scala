7347d9f234259b54cb5fafdbe6dd413bdc72c36c
provalentin <provalentin@gmail.com>
2018-05-22 18:08:53 +0300

    Update GreetingActorExample.scala

--- src/main/scala/com/goticks/GreetingActorExample.scala
+++ src/main/scala/com/goticks/GreetingActorExample.scala
@@ -1,15 +1,13 @@
 import akka.actor._
-
 case class WhoToGreet(who: String)
-
 class GreetingActor extends Actor {
-  receive = {
+  def receive = {
     case WhoToGreet(who) => println(s"Hello ${who}")
   }
 }
-
 object GreetingActorExample extens App {
   val system = ActorSystem("Hello-Akka")
-  val greeter = system.actorOf(Props["GreetingActor"])
-  greeter ! WhoToGreet("Hello scala")
+  val greeter = system.actorOf(Props["GreetingActor"], "greeter")
+  greeter ! WhoToGreet("Hello-Scala")
+  sytem.terminate()
 }
